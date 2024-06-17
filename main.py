78fb38b2e7b866304c7694b52e31d80b9388b93a
Longbin Chen <longbin.chen.2019@gmail.com>
2024-06-17 08:32:15 -0700

    modify the code to add more commands

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from string import Template
+import re
+import json
 
 import httpx
 from pynput import keyboard
@@ -9,51 +11,78 @@ import pyperclip
 
 controller = Controller()
 
-OLLAMA_ENDPOINT = "http://localhost:11434/api/generate"
+OLLAMA_ENDPOINT = "http://127.0.0.1:11434/api/generate"
 OLLAMA_CONFIG = {
-    "model": "mistral:7b-instruct-v0.2-q4_K_S",
+    "model": "llama3:instruct",
     "keep_alive": "5m",
     "stream": False,
 }
 
-PROMPT_TEMPLATE = Template(
+template_dic = dict()
+template_dic['fix']  = Template(
     """Fix all typos and casing and punctuation in this text, but preserve all new line characters:
+Return only the corrected text, don't include a preamble or any note.
+text: $text
+"""
+)
+template_dic['explain']  = Template(
+    """Write a short description to explain the following concept. don't include any preamble or any note on how to generate it. please only return the explaination. 
+text: $text
+""")
 
-$text
-
-Return only the corrected text, don't include a preamble.
+template_dic['rephrase']  = Template(
+    """Please rephrase the following text. Don't include any preamble or any note on how to generate it. please only return the explaination. 
+text: $text
+""")
+template_dic['longer']  = Template(
+    """Please elaborate and enrich the following text. Don't include any preamble or any note on how to generate it. please only return the explaination. 
+text: $text
 """
 )
 
 
-def fix_text(text):
-    prompt = PROMPT_TEMPLATE.substitute(text=text)
+template_dic['shorter']  = Template(
+    """Please make a shorter summary of the following text. Don't include any preamble or any note on how to generate it. please only return the explaination. 
+text: $text
+"""
+)
+
+template_dic['empty']  = Template(
+    """$text"""
+)
+
+
+def process_text(text, command_str):
+    template_str = template_dic.get(command_str, template_dic['empty'])
+    prompt = template_str.substitute(text=text)
+    print(prompt)
     response = httpx.post(
         OLLAMA_ENDPOINT,
         json={"prompt": prompt, **OLLAMA_CONFIG},
         headers={"Content-Type": "application/json"},
-        timeout=10,
+        timeout=100,
     )
     if response.status_code != 200:
         print("Error", response.status_code)
         return None
+    print('response ----')
+    print(response.json()["response"].strip())
+    print("----")
     return response.json()["response"].strip()
 
+# parse the text where text is a command followed by comment text. a command is a word start with #, and it should be one of the keys in template_dic.
+def parse_text(text):
+    #regular expression to parse the command
+    text = text.strip()
+    if text.startswith("#"):
+       x = text.split(" ", 1)
+       command = x[0][1:]
+       com_text = "" if len(x) == 1 else x[1]
+       return command, com_text
+       
+    return "", text
 
-def fix_current_line():
-    # macOS short cut to select current line: Cmd+Shift+Left
-    controller.press(Key.cmd)
-    controller.press(Key.shift)
-    controller.press(Key.left)
-
-    controller.release(Key.cmd)
-    controller.release(Key.shift)
-    controller.release(Key.left)
-
-    fix_selection()
-
-
-def fix_selection():
+def process_selection():
     # 1. Copy selection to clipboard
     with controller.pressed(Key.cmd):
         controller.tap("c")
@@ -61,11 +90,13 @@ def fix_selection():
     # 2. Get the clipboard string
     time.sleep(0.1)
     text = pyperclip.paste()
-
+    command_str, command_text = parse_text(text)
+    print("command is : ", command_str)
     # 3. Fix string
     if not text:
         return
-    fixed_text = fix_text(text)
+    
+    fixed_text = process_text(command_text, command_str)
     if not fixed_text:
         return
 
@@ -79,12 +110,10 @@ def fix_selection():
 
 
 def on_f9():
-    fix_current_line()
+    #fix_current_line()
+    process_selection()
 
 
-def on_f10():
-    fix_selection()
 
-
-with keyboard.GlobalHotKeys({"<101>": on_f9, "<109>": on_f10}) as h:
+with keyboard.GlobalHotKeys({"<101>": on_f9}) as h:
     h.join()
